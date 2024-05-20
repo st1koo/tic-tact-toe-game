7b3008c3f6112b8d30a42442470c4197eb31c490
st1koo <antonluomala2008@gmail.com>
2024-05-20 13:36:39 +0300

    started to get player values

---
+++ main.js
@@ -0,0 +1,9 @@
+//function called whenever user tab on anybox
+function myfunc() {
+
+    //setting dom to all boxes or input field
+    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    b1 = document.getElementById(b1).value;
+    b2 = document.getElementById(b2).value;
+    b3 = document.getElementById(b3).value;
+
