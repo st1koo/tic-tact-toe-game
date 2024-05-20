9f02168a00ac7e72bd782d7d9fe8bad242fb1715
st1koo <antonluomala2008@gmail.com>
2024-05-20 13:41:34 +0300

    Added all values

--- main.js
+++ main.js
@@ -6,4 +6,9 @@ function myfunc() {
     b1 = document.getElementById(b1).value;
     b2 = document.getElementById(b2).value;
     b3 = document.getElementById(b3).value;
-
+    b4 = document.getElementById(b4).value;
+    b5 = document.getElementById(b5).value;
+    b6 = document.getElementById(b6).value;
+    b7 = document.getElementById(b7).value;
+    b8 = document.getElementById(b8).value;
+    b9 = document.getElementById(b9).value;
